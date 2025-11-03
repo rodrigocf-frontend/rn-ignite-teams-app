63b469ea16bf863161d5e244b6aae392cb7ee2c0
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2025-11-03 15:45:25 -0300

    feat: add route navigation to GroupItem

--- src/components/GroupItem/index.tsx
+++ src/components/GroupItem/index.tsx
@@ -2,6 +2,7 @@ import { UsersThreeIcon } from "phosphor-react-native";
 import { Container, Title } from "./styles";
 import { useTheme } from "styled-components/native";
 import { Group } from "../../store/GroupsContext";
+import { useNavigation } from "@react-navigation/native";
 
 interface Props {
   data: Group;
@@ -9,9 +10,10 @@ interface Props {
 
 export function GroupItem({ data }: Props) {
   const theme = useTheme();
+  const navigation = useNavigation();
 
   return (
-    <Container>
+    <Container onPress={() => navigation.navigate("Group", { id: data.id })}>
       <UsersThreeIcon size={31} weight="fill" color={theme.color.GREEN_500} />
       <Title>{data.name}</Title>
     </Container>
