3a3d93d29233d88632e85f2fcc7d9cde1524249b
Rodrigo Cunha <rodrigoc.frontend@gmail.com>
2025-11-03 16:47:10 -0300

    feat: add service to pesist app data on local storage

---
+++ src/services/group.ts
@@ -0,0 +1,36 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Group } from "../store/GroupsContext";
+import { AppError } from "../utils/error-handlers";
+
+type GroupStorageData = {
+  groups: Group[];
+  lastPlayerId: number;
+  lastGroupId: number;
+};
+
+export const getGroups = async () => {
+  try {
+    AppError("");
+    const data = await AsyncStorage.getItem("@groups/data");
+
+    if (data) {
+      const dataToJSON = JSON.parse(data) as GroupStorageData;
+      return dataToJSON;
+    }
+    return {
+      groups: [],
+      lastGroupId: 0,
+      lastPlayerId: 0,
+    };
+  } catch (e) {
+    return AppError(e);
+  }
+};
+
+export const saveGroups = async (data: GroupStorageData) => {
+  try {
+    await AsyncStorage.setItem("@groups/data", JSON.stringify(data));
+  } catch (e) {
+    return AppError(e);
+  }
+};
